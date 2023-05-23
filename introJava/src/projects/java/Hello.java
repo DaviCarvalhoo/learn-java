99d886928e995074d1f06eed0bdb33817e3bd8c8
DaviCarvalhoo <davicarvalhotech@gmail.com>
2023-05-23 20:30:43 -0300

    studies about var in java

---
+++ introJava/src/projects/java/Hello.java
@@ -0,0 +1,13 @@
+package projects.java;
+public class Hello {
+    public static void main(String[] args) throws Exception {
+        // create a var in java
+        String mundo = "Mundo";
+        System.out.println("Olá, "+mundo+"!");
+        int n1 = 2;
+        int n2;
+        n2 = 3;
+        int soma = n1 + n2;
+        System.out.println(soma);
+    }
+}
